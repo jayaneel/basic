db923a457620a2cf6dbc85246c2510e29893673c
jayaneel <83657077+jayaneel@users.noreply.github.com>
2021-07-08 23:28:30 +0530

    Update LifeCycleServlet.java

--- src/main/java/LifeCycleServlet.java
+++ src/main/java/LifeCycleServlet.java
@@ -38,7 +38,7 @@ public class LifeCycleServlet implements Servlet {
 	{
 		res.setContentType("text/html");
 		PrintWriter pw = res.getWriter();
-		pw.println("<h2>Did you recieve notificatons??</h2>");
+		pw.println("<h2>Recieved notificatons :-)</h2>");
 		System.out.println("in service");
 	}
 
